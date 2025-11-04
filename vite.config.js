ab72188b98e50b6f891692e6162c89d47e9b391e
Bernd <berndschaller@gmx.net>
2025-11-04 11:58:15 +0100

    Add Tailwind CSS integration and update styles
    - Added Tailwind CSS and its Vite plugin to the project dependencies.
    - Updated package.json to include Tailwind CSS and its Vite plugin.
    - Modified index.css to incorporate Tailwind CSS styles and improve formatting.
    - Enhanced button styles and layout adjustments for better responsiveness.
    - Cleaned up CSS by removing unnecessary whitespace and comments.

--- vite.config.js
+++ vite.config.js
@@ -1,9 +1,12 @@
 import { defineConfig } from 'vite'
 import react from '@vitejs/plugin-react'
+import tailwindcss from '@tailwindcss/vite'
 
 const isDev = process.env.NODE_ENV !== 'production'
 
 export default defineConfig({
-  plugins: [react()],
+  plugins: [
+    react(),
+    tailwindcss(),],
   base:  '/', 
 })
